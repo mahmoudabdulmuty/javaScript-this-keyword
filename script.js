df5aed011f1942e98b51d330d6df8db44f4cb267
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-28 10:11:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log(this);
+// (this = Window-Object) -> (this) value in the global scope
+
+const regularFun = function () {
+	console.log(this);
+	// Regular Functions has it's own (this)
+	// (this = undefined) in (strict mode) Otherwise, (this = Window-Object)
+};
+regularFun();
+
+const arrow = () => {
+	console.log(this);
+	// Arrow Functions doesn't have it's own (this)
+	// arrow function takes (this) value from parent scope (which is global in this case)
+	// so (this = Window-Object) in this case
+};
+arrow();
+
+document.querySelector('h1').onclick = function () {
+	console.log(this);
+	// this = Dom Element that the handler attached to
+};
+
+const obj = {
+	firstName: 'Mahmoud',
+	method() {
+		console.log(this); // this = object calling the method
+	}
+};
+
+obj.method();
