6cd96168b48bd8b221a98c1a0c399e4a964ee979
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-11-06 23:12:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// 1. this in the Global Scope  
 console.log(this);
 // (this = Window-Object) -> (this) value in the global scope
 
+// 2. this value in Regular Functions
 const regularFun = function () {
 	console.log(this);
 	// Regular Functions has access to (this)
@@ -10,6 +12,7 @@ const regularFun = function () {
 };
 regularFun();
 
+// 3. this value in Arrow Functions
 const arrow = () => {
 	console.log(this);
 	// Arrow Functions doesn't have access to (this)
@@ -18,15 +21,18 @@ const arrow = () => {
 };
 arrow();
 
-document.querySelector('h1').onclick = function () {
+// 4. this value in Event listener passed function
+document.querySelector('h1').addEventListener('click' , function () {
 	console.log(this);
 	// this = Dom Element that the handler attached to
 };
-
+					      
+// 5. this value in Object Method
 const obj = {
 	firstName: 'Mahmoud',
 	method() {
-		console.log(this); // this = object calling the method
+		console.log(this); 
+		// this = object calling the method
 	}
 };
 
